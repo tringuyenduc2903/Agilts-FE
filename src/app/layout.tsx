7d20945f6dd9af17a2b2b2a4935f093c4f3e5664
quanber123 <quantm103@gmail.com>
2024-05-30 16:02:21 +0700

    [feat] structure i18n

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from 'next';
 import { Rajdhani } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/common/Header/Header';
-
+import { I18nextProvider } from 'react-i18next';
+import i18n from '@/lib/i18n/i18n';
 const inter = Rajdhani({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
@@ -21,8 +22,10 @@ export default async function RootLayout({
   return (
     <html lang='en'>
       <body className={`${inter.className} h-screen`}>
-        <Header />
-        {children}
+        <I18nextProvider i18n={i18n}>
+          <Header />
+          {children}
+        </I18nextProvider>
       </body>
     </html>
   );

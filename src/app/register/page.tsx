f6f55c37315c65b12d811ee35e9f74066f65bec0
quanber123 <quantm103@gmail.com>
2024-05-30 14:29:01 +0700

    [fix] remove unused

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 import bgLogo from '@/assets/h4-slider-img-1.jpg';
 import Image from 'next/image';
 import { useForm, SubmitHandler } from 'react-hook-form';

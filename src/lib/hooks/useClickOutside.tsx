2d62de12eff1bb2b009321fe6b3ba6f87cf315ba
quanber123 <quantm103@gmail.com>
2024-06-11 01:06:33 +0700

    [feat] add useClickOutside hook

---
+++ src/lib/hooks/useClickOutside.tsx
@@ -0,0 +1,42 @@
+import { useCallback, useRef, useEffect } from 'react';
+
+type Props = () => void;
+
+const useClickOutside = (cb: Props) => {
+  const sectionRef = useRef<HTMLElement | HTMLDivElement | null>(null);
+
+  const clickOutside = useCallback(
+    (e: MouseEvent) => {
+      if (
+        sectionRef.current &&
+        !sectionRef.current.contains(e.target as Node)
+      ) {
+        cb();
+      }
+    },
+    [cb]
+  );
+
+  const handleKeyPress = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        cb();
+      }
+    },
+    [cb]
+  );
+
+  useEffect(() => {
+    // document.addEventListener('mousedown', clickOutside);
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      // document.removeEventListener('mousedown', clickOutside);
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [clickOutside, handleKeyPress]);
+
+  return { sectionRef, clickOutside };
+};
+
+export default useClickOutside;

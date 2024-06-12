2da046cf62cd9dda131d380c80bd8388175a85c0
quanber123 <quantm103@gmail.com>
2024-06-12 10:00:40 +0700

    [fix] remove some link unused

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import dynamic from 'next/dynamic';
 import React, { useContext, useEffect, useMemo, useState } from 'react';
 import { FetchDataContext } from '@/contexts/FetchDataProvider';
 import { useRouter } from 'next/navigation';

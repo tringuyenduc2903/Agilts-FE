f906900f01b3bb459e76cb1dc5ee9846dede6477
quanber123 <quantm103@gmail.com>
2024-06-18 22:18:12 +0700

    [style] fix font-size

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -18,7 +18,9 @@ export default function Error({
     <html lang={getCookies()?.NEXT_LOCALE || 'vi'}>
       <body>
         <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-neutral-800 flex flex-col items-center gap-4'>
-          <h2 className='text-4xl font-bold'>Something went wrong!</h2>
+          <h2 className='text-2xl md:text-4xl font-bold'>
+            Something went wrong!
+          </h2>
           <button
             className='bg-neutral-800 text-white px-4 py-2 rounded-sm'
             onClick={() => reset()}

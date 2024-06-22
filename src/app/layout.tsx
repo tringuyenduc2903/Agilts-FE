6f460b4df6f7a6d80bea8e74339bcfa85eb45a95
quanber123 <quantm103@gmail.com>
2024-06-23 02:38:57 +0700

    [fix] fix about viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { cookies } from 'next/headers';
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
+  // maximumScale: 1,
+  // userScalable: false,
   // Also supported by less commonly used
   // interactiveWidget: 'resizes-visual',
 };

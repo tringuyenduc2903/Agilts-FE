16adf081ba3cfcae416d9513da4615a51d8fef67
quanber123 <quantm103@gmail.com>
2024-06-23 10:04:10 +0700

    [fix] remove cspHeader

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,6 @@
 /** @type {import('next').NextConfig} */
 import createNextIntlPlugin from 'next-intl/plugin';
-const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
-console.log(backendUrl);
 const withNextIntl = createNextIntlPlugin();
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' ${backendUrl} 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
-const nextConfig = {
-  headers: async () => {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
-          },
-        ],
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);

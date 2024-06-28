e1ad7acfcf30779f6bd551a44b9a40893dae4654
quanber123 <quantm103@gmail.com>
2024-06-29 00:31:14 +0700

    [demo] ServiceWorker - log url to browser to test

--- public/config-sw.js
+++ public/config-sw.js
@@ -1,20 +1,32 @@
 const CACHE_NAME = 'next-agilts-staging-v2.2';
 
-const cacheClone = async (e) => {
-  const res = await fetch(e.request);
-  const resClone = res.clone();
+const cacheClone = async (event) => {
+  try {
+    const response = await fetch(event.request);
+    const responseClone = response.clone();
 
-  const cache = await caches.open(CACHE_NAME);
-  await cache.put(e.request, resClone);
-  return res;
+    const cache = await caches.open(CACHE_NAME);
+    await cache.put(event.request, responseClone);
+
+    return response;
+  } catch (error) {
+    console.error('Fetch failed; returning offline page instead.', error);
+    const cache = await caches.open(CACHE_NAME);
+    const cachedResponse = await cache.match(event.request);
+    return cachedResponse || Promise.reject('no-match');
+  }
 };
 
 const fetchEvent = () => {
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      cacheClone(e)
-        .catch(() => caches.match(e.request))
-        .then((res) => res)
+  self.addEventListener('fetch', (event) => {
+    const url = new URL(event.request.url);
+    console.log(url);
+    if (url.pathname.startsWith('/api')) {
+      return;
+    }
+
+    event.respondWith(
+      cacheClone(event).catch(() => caches.match(event.request))
     );
   });
 };

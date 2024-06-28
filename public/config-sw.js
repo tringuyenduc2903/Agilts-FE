2c7bbbc3a14f1ea0a3ceaebdc9fa7daa3abbe1c0
quanber123 <quantm103@gmail.com>
2024-06-28 18:15:22 +0700

    [fix] ServiceWorkers - fix error about "cant access this website"

--- public/config-sw.js
+++ public/config-sw.js
@@ -10,8 +10,14 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event?.request).then((response) => {
-      return response || fetch(event?.request);
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch((error) => {
+          console.error('Fetch failed:', error);
+          throw error;
+        })
+      );
     })
   );
 });
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,18 @@
 function registerServiceWorker() {
   if (typeof window !== 'undefined') {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/config-sw.js').then((registration) => {
-        console.log('Service Worker registration successful:', registration);
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/config-sw.js').then(
+          (registration) => {
+            console.log(
+              'Service Worker registered with scope:',
+              registration.scope
+            );
+          },
+          (error) => {
+            console.error('Service Worker registration failed:', error);
+          }
+        );
       });
     }
   }
